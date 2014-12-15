778c13cb3746540936a4ed67d5a72019ff943135
Dallin Christensen <dchristensen@izeni.com>
2014-12-15 10:30:41 -0700

    Remove post migrate hook

--- django_balanced/listeners.py
+++ django_balanced/listeners.py
@@ -2,17 +2,12 @@ from __future__ import unicode_literals
 
 from django.dispatch import receiver
 from django.db.models import signals
+from django.contrib.auth import get_user_model
 
-try:
-    from django.contrib.auth import get_user_model
-except ImportError:
-    from django.contrib.auth.models import User
-    get_user_model = lambda: User
-
-try:
-    post_sync = signals.post_migrate
-except AttributeError:
-    post_sync = signals.post_syncdb
+# try:
+#     post_sync = signals.post_migrate
+# except AttributeError:
+#     post_sync = signals.post_syncdb
 
 
 # This will create an account per user when they are next saved. Subsequent
@@ -23,9 +18,12 @@ def create_user_profile(sender, instance, created, **kwargs):
     Account.objects.get_or_create(user=instance)
 
 
-# Sync with Balanced when the database is synced
-@receiver(post_sync, dispatch_uid='django_balanced.listeners.sync_balanced')
-def sync_balanced(sender, **kwargs):
-    from .models import BankAccount, Credit
-    BankAccount.sync()
-    Credit.sync()
+# We are disabling this because it makes migrations take too long.
+# Instead, use the sync management command.
+
+# # Sync with Balanced when the database is synced
+# @receiver(post_sync, dispatch_uid='django_balanced.listeners.sync_balanced')
+# def sync_balanced(sender, **kwargs):
+#     from .models import BankAccount, Credit
+#     BankAccount.sync()
+#     Credit.sync()
